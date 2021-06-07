c0bcc6c918e199956c99c9957cd7c30dee0d4144
altinbekpin <altinbekpin@gmail.com>
2021-06-07 18:31:44 +0600

    locales biriktirdim

---
+++ src/components/contragent/Organizations.vue
@@ -0,0 +1,117 @@
+<template>
+<div>
+  <div class="content-section introduction">
+	<div class="feature-intro p-ml-3" >
+    <h4 style="display:inline">{{$t('contragent.organization')}}</h4>
+		<label style="color:grey">&nbsp;{{this.$t('common.records')}}: {{this.count}}</label>
+	</div>
+	</div>
+	<div class="content-section implementation">
+	<div class="card p-p-0">
+	 	<div class="p-col">
+			<Menubar :model="menu" :key="active" style="height:36px;margin-top:-7px; margin-right: -7px;margin-left: -7px;" >
+				<template #end>
+					<span class="p-input-icon-left">
+            <i class="pi pi-search" />
+            <InputText style="height:30px" v-model="filters['global']" placeholder="іздеу" />
+          </span>
+				</template>
+			</Menubar>
+      <div class="box">
+				<DataTable class="p-datatable-sm" v-model:selection="selectedOrganizations" selectionMode="single" dataKey="id" :value="organizations" :paginator="true" :rowHover="true" :rows="orgShowCount" :filters="filters"
+				:loading="loading">
+					<template #empty>
+						{{this.$t('common.recordsNotFound')}}
+					</template>
+					<template #loading>
+						{{this.$t('common.recordsLoading')}}
+					</template>
+					<Column selectionMode="multiple" headerStyle="width: 3em"></Column>
+					<Column field="name" :header="$t('common.name')" :sortable="true">
+						<template #body="slotProps">
+            	<Button class="p-button-link p-text-left" @click="toggle($event,slotProps.data)">{{(this.$i18n.locale != 'ru' ? '"' + slotProps.data.name + '" ' + slotProps.data.form.shortname : slotProps.data.form.shortnamerus + ' "' + slotProps.data.namerus + '"')}}</Button>
+        		</template>
+					</Column>
+					<Column field="iin" :header="$t('contact.bin')" :sortable="true"></Column>
+					<Column field="locality.name" :header="$t('contact.locality')" :sortable="true"></Column>
+					<Column field="phone" :header="$t('contact.phone')" :sortable="true"></Column>
+				</DataTable>
+				<Sidebar v-model:visible="sideVisible" position="right" class="p-sidebar-lg" style="overflow-y:scroll">
+					<Organization :modelValue="currentOrganization" :readonly="true"></Organization>
+				</Sidebar>
+
+
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
+</template>
+<script>
+	import {apiDomain} from "@/config/config";
+  import axios from 'axios';
+	import Organization from './Organization.vue';
+	import Enum from "@/enum/docstates/index"
+
+
+	export default {
+  components: { Organization },
+    data() {
+        return {
+						active: null,
+						organizations: null,
+						count:0,
+						selectedOrganizations: null,
+						currentOrganization: {},
+						orgShowCount : 15,
+						loading: true,
+						sideVisible : false,
+						filters: {},
+            menu: [
+                {
+                  label:'',
+                  icon:'pi pi-fw pi-refresh',
+									command: () => {
+                  	this.$toast.add({severity:'success', summary:'Updated', detail:'Data Updated', life: 3000});
+                  }
+                },
+                {
+                   label: this.$t('bank.card'),
+                   icon:'pi pi-fw pi-id-card',
+                },
+                {
+                   label:this.$t('common.contacts'),
+                   icon:'pi pi-fw pi-user',
+                },
+             ]
+        }
+    },
+		props: {
+    	modelValue: null
+		},
+		methods: {
+			initApiCall(){
+        let url = "/contragent/organizations";
+
+				var req = {"id" : 0, "count": this.orgShowCount, "orgtype" : Enum.ContragentType.Bank + Enum.ContragentType.Organization };
+        axios.post(apiDomain+url, req)
+        .then(res=>{
+
+          this.organizations =  res.data
+        })
+        .catch(error => {
+            console.error(error)
+        })
+      },
+			toggle(event, data) {
+						this.currentOrganization = data;
+						this.selectedOrganizations = data;
+						this.sideVisible = true;
+      },
+    },
+		mounted() {
+      this.initApiCall();
+			this.loading = false
+    },
+	}
+</script>
---
+++ src/components/contragent/TopMenuBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Menubar :model="menu" :key="active" style="height:36px;margin-top:-7px;margin-left:-14px;margin-right:-14px"></Menubar>
+  </div>
+</template>
+<script>
+export default {
+	 data() {
+		return {
+            active: null,
+			menu: [
+        {
+          label:this.$t('common.save'),
+          icon:'pi pi-fw pi-save',
+          disabled: this.readonly,
+					command: () => {
+          	this.$toast.add({severity:'success', summary:'Updated', detail:'Data Updated', life: 3000});
+          }
+        },
+        {
+           label: this.$t('contact.mailto'),
+           icon:'pi pi-fw pi-envelope',
+					 command: () => {
+						 window.location.href = "mailto:" + this.organization.email;
+					 }
+        },
+        {
+           label: this.$t('contragent.dossier'),
+           icon:'pi pi-fw pi-copy',
+        },
+        {
+           label:this.$t('common.contacts'),
+           icon:'pi pi-fw pi-user',
+        },
+      ]
+    }
+   },
+    props: {
+      organization: null,
+      readonly: Boolean,
+  },
+}
+
+</script>
