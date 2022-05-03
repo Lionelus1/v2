8cce37dc9c1407120fedebbdc5d6a4c1bf77de02
berikboltt@gmail.com <berikboltt@gmail.com>
2022-05-03 15:40:35 +0600

    d

--- src/config/config.js
+++ src/config/config.js
@@ -6,15 +6,9 @@ export const hdfsApi = "http://localhost:8085";
 export const signerApi = "https://smart.enu.kz:6990"
 //export const signerApi = "http://10.1.1.161:6990"
 //export const smartEnuApi = "https://smart.enu.kz:8081"
-<<<<<<< HEAD
 export const smartEnuApi = "http://smart.enu.kz:8090"
 //export const smartEnuApi = "https://10.1.2.75:8081"
 //export const smartEnuApi = "http://192.168.137.151:8081"
-
-=======
-//export const smartEnuApi = "http://smart.enu.kz:8090"
-export const smartEnuApi = "http://localhost:8080"
->>>>>>> work_plan
 export const templateApi = "http://localshost:8082"
 
 export const header  = {
