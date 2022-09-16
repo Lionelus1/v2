7892e2059df077a192cf1a084d0b6be39f47900d
berikboltt@gmail.com <berikboltt@gmail.com>
2022-09-16 16:50:26 +0600

    final

--- src/components/OutQr.vue
+++ src/components/OutQr.vue
@@ -4,47 +4,67 @@
       <h1>Smart.enu жүйесіне қош келдіңіз.</h1>
   </div> -->
   <div class="grid">
-    <div class="col-12">
-      <div class="card">
-        
-        <h5 class="p-text-center p-text-uppercase">Smart.enu жүйесіне қош келдіңіз.</h5>
+    
+    <div class="col-6">
+      <div class="card p-text-center p-text-info">      
+        <h5>SMART.ENU қосымшасы арқылы QR сканерлеңіз!</h5>
+        <h5>Используйте QR сканер приложения SMART.ENU!</h5>
+        <h5>Use the QR scanner of the SMART.ENU app!</h5>
         <Avatar icon="pi pi-user" class="mr-2" size="xlarge" />
         <Avatar icon="pi pi-user" class="mr-2" size="large" style="background-color:#2196F3; color: #ffffff"/>
         <Avatar icon="pi pi-user" class="mr-2" style="background-color:#9c27b0; color: #ffffff" />
+        
       </div>
     </div>
   </div>
-  <div class="grid">
-    <div class="col-12 p-text-center">
-        <img src="/assets/layout/images/logo_eng.png" alt="Image" width="250"/>
+  <div class="car p-text-center">
+        
     </div>
-  </div>
   <div class="p-fluid p-formgrid p-grid p-jc-center p-pt-2">
-      the qr code gen here
+      
+      <qrcode-vue size="500" render-as="svg" margin="2" :value="qrCode"></qrcode-vue>
   </div>
 </template>
 
 <script>
 
   import axios from 'axios';
-  import {getHeader,header, smartEnuApi,etspTokenEndPoint} from "../config/config";
-  
+  import {getHeader,header, smartEnuApi,etspTokenEndPoint,socketApi} from "../config/config";
+  import QrcodeVue from "qrcode.vue";
 
   const authUser = {};
   export default {
        name : "OutQr",
+       components: {QrcodeVue},
        data(){
         return {
-          
+          qrCode:"",
+          connection:null,
+          gateName:"outGate_main"
         }
       },
-      created() {
-        //alert("welcome qr out gate");
-        
-      },
+      
       methods:{
+          connect() {
+            let t = this;
+            this.connection =new WebSocket(socketApi+'/ws');
+            
+
+            this.connection.onmessage = function(data) {
+                t.qrCode = data.data;
+            }
+            
+            this.connection.onopen = function(event) {
+                let d = {"key":"somekey", "value":"someid"};
+                t.connection.send(t.gateName);
+            }
+        },
+       
+    },
+    mounted() {
         
-      }
+        this.connect();
+    }
   }
 </script>
 
