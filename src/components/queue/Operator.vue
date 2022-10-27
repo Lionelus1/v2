2505025c476291f0b198b4e6a240bfdb9903f6f5
Beybaris <mc.beibarys@gmail.com>
2022-10-27 10:49:07 +0600

    fix

--- src/components/queue/Operator.vue
+++ src/components/queue/Operator.vue
@@ -1,5 +1,4 @@
 <template>
-{{service}}
   <ProgressBar v-if="loading" mode="indeterminate" style="height: .5em;" />
   <div class="card summary">
     <h3>{{ (this.queue != null ? this.queue['queueName' + $i18n.locale]:"")}}</h3>
