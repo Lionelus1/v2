59929139fb16736b565dfcbbc9e2c249547c596c
Beybaris <mc.beibarys@gmail.com>
2022-11-28 12:43:00 +0600

    fix signature info

--- src/components/ncasigner/SignatureQrPdf.vue
+++ src/components/ncasigner/SignatureQrPdf.vue
@@ -6,7 +6,7 @@
       <div id="qr" v-for="(item, index) of signatures" :key="index"
            style="border: 1px solid #000; padding: 5px; margin: 5px;display: block;">
         <p v-if="item.user"><b>{{ item.user.fullName }}</b> ({{ $t('ncasigner.IIN') }} <em>{{ item.user.IIN }}</em>)
-          <br/> {{ item.sign ? $t('ncasigner.signed') + ": " + new Date(item.signDate).toLocaleDateString() + ", " + new Date(item.signDate).toLocaleTimeString()  : $t('ncasigner.signingexpected') }}
+          <br/> {{ item.sign && item.sign.length > 0 ? $t('ncasigner.signed') + ": " + new Date(item.signDate).toLocaleDateString() + ", " + new Date(item.signDate).toLocaleTimeString()  : $t('ncasigner.signingexpected') }}
         </p>
         <div style="width: 100%;text-align: left;">
           <qrcode-vue v-for="(i, ind) of item.sign" :key="ind" size="300" render-as="svg" margin="2" :value="i"></qrcode-vue>
