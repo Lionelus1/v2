c87fd18c370cf558ee23570b2840946d680ed9ba
sultan.b <mc.beibarys@gmail.com>
2023-03-20 18:41:24 +0600

    tinymce custom file upload

--- src/components/TinyEditor.vue
+++ src/components/TinyEditor.vue
@@ -1,5 +1,5 @@
 <template>
-  <editor :api-key="editorApi" v-model="content" :init="editorOptions"/>
+  <editor ref="myEditor" :id="'tiny'" :api-key="editorApi" v-model="content" :init="editorOptions"/>
 </template>
 
 <script>
@@ -36,7 +36,18 @@ export default {
             bullist numlist outdent indent | removeformat | table | image media | code ',
         images_upload_handler: uploadSingFile,
         language: this.$i18n.locale === "en" ? "en_US" : this.$i18n.locale === "kz" ? "kk" : this.$i18n.locale,
-        content_style: "body { font-size: 14px; }"
+        content_style: "body { font-size: 14px; }",
+        file_picker_callback: this.uploadFile,
+        setup: editor => {
+          editor.ui.registry.addButton('customUpload', {
+            text: '<i class="fa-solid fa-file-arrow-up" style="font-size: 20px;"></i>',
+            onAction: () => {
+              // Open file upload form
+              //this.$refs.myEditor.click();
+              //window.tinymce.activeEditor.execCommand('mceInsertContent', false, `<ul><li>Custom FIle YPLOAD</li><li>Custom FIle YPLOAD</li><li>Custom FIle YPLOAD</li></ul>`);
+            }
+          });
+        }
       },
       editorApi: "60lj0ro6ojutgtjvqom1a5txsxm2azkl8pftmzhf8ddim86d",
     }
@@ -45,7 +56,29 @@ export default {
     value(newValue) {
       this.content = newValue;
     }
+  },
+  methods: {
+    uploadFile(cb, value, meta) {
+      const input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      //event listeners
+      input.addEventListener('change', (e) => {
+        const file = e.target.files[0];
 
+        const reader = new FileReader();
+        reader.addEventListener('load', () => {
+          //handle data processing with a blob
+          const id = 'blobid' + (new Date()).getTime();
+          const blobCache = window.tinymce.activeEditor.editorUpload.blobCache;
+          const base64 = reader.result.split(',')[1];
+          const blobInfo = blobCache.create(id, file, base64);
+          blobCache.add(blobInfo);
+          cb(blobInfo.blobUri(), { title: file.name });
+        });
+        reader.readAsDataURL(file);
+      });
+      input.click();
+    }
   }
 }
 </script>
