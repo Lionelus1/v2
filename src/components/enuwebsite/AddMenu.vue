4986ea13e5cfec215c2a3b47fd1dc9deb4550b3f
uaibol <uaibol@gmail.com>
2023-08-21 17:34:33 +0600

    menu add edit fixed

--- src/components/enuwebsite/AddMenu.vue
+++ src/components/enuwebsite/AddMenu.vue
@@ -20,7 +20,7 @@
                 :class="{ 'p-invalid': !formData.menu_title_en && submitted }" />
             <small v-show="!formData.menu_title_en && submitted" class="p-error">{{ $t("smartenu.titleEnInvalid") }}</small>
         </div>
-        <div class="field" v-if="!currentMenu">
+        <div class="field" v-if="!menu_id">
             <label>{{ $t('web.menuParent') }}</label>
             <TreeSelect v-model="selectedTreeMenu" :options="menus" :placeholder="$t('common.choose')"
                 @node-expand="expandMenuTreeSelect" @nodeSelect="menuTreeSelect"></TreeSelect>
@@ -435,4 +435,4 @@ export default {
 .text-underline {
     text-decoration: underline;
 }
-</style>
+</style>formData
