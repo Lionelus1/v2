b68e799f188211f22d62d5f7320f9cfd93039794
saulet <mega.saulet@list.ru>
2024-01-26 15:57:48 +0600

    add: position management, rolePosition

--- src/components/roleControl/OrgControl.vue
+++ src/components/roleControl/OrgControl.vue
@@ -8,14 +8,18 @@
 		<TabPanel :header="$t('roleControl.rolePosRelTitle')">
 			<RolePositionRel></RolePositionRel>
 		</TabPanel>
+		<TabPanel :header="$t('roleControl.rolePosTitle')">
+			<RolePosition></RolePosition>
+		</TabPanel>
 	</TabView>
 	</div>
 </template>
 <script>
 import RoleControl from './RoleControl.vue';
 import RolePositionRel from './RolePositionRel.vue';
+import RolePosition from './RolePosition.vue'
 
 export default {
-	components: { RoleControl, RolePositionRel },
+	components: { RoleControl, RolePositionRel,RolePosition },
 }
 </script>
