356c7dbceadc733ff30558a53f56f1becd281530
Andakov Dilmukhan <andakovdima@mail.ru>
2024-03-29 12:21:50 +0600

    fix queue

--- src/components/queue/QueueQR.vue
+++ src/components/queue/QueueQR.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="card text-center flex flex-column gap-4">
+  <div class="card text-center flex flex-column gap-4 m-auto" style="width: 385px">
     <h3>{{$t('contact.phone')}}</h3>
     <InputMask class="p-inputtext-lg" @input="validatePhoneNumber" id="mobile" mask="+7-(999)-999-99-99" />
     <Button class="justify-content-center p-button-lg">{{$t('common.continue')}}</Button>
     <small v-if="validationError" class="p-error">{{ $t("error")}}</small>
   </div>
-<div class="card m-auto" style="width: 385px">
-  <Button class="p-button-lg mb-3" style="width: 100%" v-for="i of queues" :key="i">{{i.queueNamekz}}</Button>
+<div class="card m-auto mt-4" style="width: 385px">
+  <a class="block mb-2 cursor-pointer" @click="getQueue()">
+    <i class="pi pi-arrow-left"></i>
+    Артқа
+  </a>
+  <Button class="p-button-lg mb-3" style="width: 100%" v-for="i of queues" :key="i" @click="getQueue(i.key)">{{i.queueNamekz}}</Button>
 </div>
 </template>
 
@@ -27,9 +31,9 @@ const validatePhoneNumber = () => {
   }
 };
 
-const getQueue = () => {
+const getQueue = (data) => {
   axios
-      .post(smartEnuApi + "/queue/allQueues", {}, {
+      .post(smartEnuApi + "/queue/allQueues", {parentID: data}, {
         headers: getHeader(),
       })
       .then((response) => {
