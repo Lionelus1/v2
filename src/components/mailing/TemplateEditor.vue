35483910c1dbb351cd9cd3d77d743093baf9b61b
nurmeden <nurmeden.02@gmail.com>
2024-08-27 10:15:31 +0500

    fix(src): delete button save

--- src/components/mailing/TemplateEditor.vue
+++ src/components/mailing/TemplateEditor.vue
@@ -169,14 +169,6 @@ export default {
     menu() {
       return [
         {
-          label: this.$t("common.save"),
-          icon: "pi pi-fw pi-save",
-          disabled: false,
-          command: () => {
-            this.sendMailing(1);
-          },
-        },
-        {
           label: this.$t("common.saveToDrafts"),
           icon: "fa-solid fa-download",
           disabled: false,
