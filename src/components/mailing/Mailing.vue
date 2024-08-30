be228715f54644401c118494ea3162b49d546879
nurmeden <nurmeden.02@gmail.com>
2024-08-30 13:39:43 +0500

    feat(src): add mailing view

--- src/components/mailing/Mailing.vue
+++ src/components/mailing/Mailing.vue
@@ -42,19 +42,34 @@
           </span>
         </template>
       </Column>
+      <Column>
+        <template #body="slotProps">
+          <ActionButton :show-label="true" :items="actions" @toggle="toggle(slotProps)" />
+        </template>
+      </Column>
     </DataTable>
   </div>
+
+
+  <MailingView v-if="mailingViewVisible" :mailingViewVisible="mailingViewVisible" :selectedMailing="selectedMailing"/>
+
+
 </template>
 
 <script>
 import api from "@/service/api";
 import { getHeader } from "@/config/config";
+import ActionButton from "@/components/ActionButton.vue";
+import MailingView from "@/components/mailing/MailingView.vue";
 
 export default {
+  components: { MailingView, ActionButton },
   data() {
     return {
       mailingList: [],
+      selectedMailing: null,
       loading: true,
+      mailingViewVisible: false,
       getParams: {
         pageNum: 1,
         itemsPerPage: 6,
@@ -78,17 +93,52 @@ export default {
     initMailing() {
       this.loading = true;
       api
-          .post("/mailing/mailingList", this.getParams, {
-            headers: getHeader(),
-          })
+          .post("/mailing/mailingList", this.getParams, { headers: getHeader() })
           .then((res) => {
             this.mailingList = res.data;
-            this.count = res.data.NewCount;
             this.loading = false;
           })
           .catch((err) => {
+            console.error("Error loading mailing list:", err);
             this.loading = false;
           });
+    },
+    async getMailingByID(mailingId) {
+      console.log("Requesting mailing with ID:", mailingId);
+      try {
+        const response = await api.post("/mailing/getMailingByID", { mailingId }, { headers: getHeader() });
+        console.log("Received response:", response.data);
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching mailing by ID:", error);
+        return null;
+      }
+    },
+    async toggle(node) {
+      const mailingId = node.data.Notification.id;
+      console.log("Toggling mailing view for ID:", mailingId);
+      this.selectedMailing = await this.getMailingByID(mailingId);
+      console.log("Selected mailing data:", this.selectedMailing);
+      if (this.selectedMailing) {
+        this.mailingViewVisible = true;
+      }
+    },
+    mailingView(actionsNode) {
+      this.actionsNode = actionsNode;
+      this.toggle(actionsNode);
+    },
+  },
+  computed: {
+    actions() {
+      return [
+        {
+          label: this.$t('common.show'),
+          icon: "fa-solid fa-eye",
+          command: () => {
+            this.mailingView(this.actionsNode);
+          },
+        },
+      ];
     },
   },
   mounted() {
--- src/components/mailing/MailingView.vue
+++ src/components/mailing/MailingView.vue
@@ -1,82 +1,49 @@
-<template>
-  <Dialog header=" "
-          v-model:visible="newsViewVisible"
-          :style="{ width: '1000px' }"
-          :modal="true"
-          class="p-fluid"
-          :closable="true"
-          :breakpoints="{'960px': '75vw', '640px': '90vw'}"
-          :close-on-escape="true"
-          @hide="closeModal"
-  >
-    <Card style="box-shadow: none">
-      <template #header>
-        <div class="dialog_img">
-          <img :src="getImageUrl(selectedNews)" style="width: 100%; height: 100%" alt=""/>
-        </div>
-      </template>
-      <template #title>
-        <div class="card_title">
-          {{
-            $i18n.locale === "kz"
-                ? selectedNews.titleKz
-                : $i18n.locale === "ru"
-                    ? selectedNews.titleRu
-                    : selectedNews.titleEn
-          }}
-        </div>
-      </template>
-      <template #subtitle>
-        {{ new Date(selectedNews.history.modifyDate).toLocaleString() }}
-      </template>
-      <template #content>
-        <div class="news_content" v-html="
-                    $i18n.locale === 'kz'
-                    ? selectedNews.contentKz
-                    : $i18n.locale === 'ru'
-                    ? selectedNews.contentRu
-                    : selectedNews.contentEn"
-        ></div>
-      </template>
-      <template #footer>
-        <div style="padding: 0 100px">
-          <img :src="selectedNews.image2" v-if="selectedNews.image2" style="width: 100%; height: 100%"/>
-        </div>
-      </template>
-    </Card>
-
-  </Dialog>
-</template>
-
 <script setup>
-import {fileRoute, smartEnuApi} from "@/config/config";
-import {inject, ref} from "vue";
-const props = defineProps(['isVisible', 'selectedNews'])
-const newsViewVisible = ref(props.isVisible ?? false)
-const emitter = inject('emitter');
+import { ref, watch } from "vue";
+const props = defineProps(['mailingViewVisible', 'selectedMailing']);
+const localMailingViewVisible = ref(props.mailingViewVisible);
+
+console.log("MailingView received selectedMailing:", props.selectedMailing);
+
+watch(() => props.mailingViewVisible, (newVal) => {
+  localMailingViewVisible.value = newVal;
+});
 
 const closeModal = () => {
-  emitter.emit("newsViewModalClose", false);
-}
-
-const getImageUrl = (data) => {
-  if (data && data.main_image_file) {
-    return  smartEnuApi + fileRoute + data.main_image_file?.filepath
-  } else if (data && data?.image1) {
-    return  smartEnuApi + fileRoute + data.image1
-  }
-}
+  localMailingViewVisible.value = false;
+};
 </script>
 
-<style lang="scss" scoped>
-.dialog_img {
-  padding: 0 100px;
-}
+<template>
+  <Dialog header="Mailing Details"
+          v-model:visible="localMailingViewVisible"
+          :style="{ width: '1000px' }"
+          modal
+          closeable
+          close-on-escape>
+    <Card style="box-shadow: none">
+      <template #header>
+        <h2>{{ props.selectedMailing?.title }}</h2>
+      </template>
+      <div>
+        <h3>Category IDs:</h3>
+        <p>{{ props.selectedMailing?.categoryIds }}</p>
 
-@media (max-width: 780px) {
-  .dialog_img {
-    padding: 0;
-  }
-}
+        <h3>Description:</h3>
+        <div v-html="props.selectedMailing?.description"></div>
 
-</style>
+        <h3>Emails:</h3>
+        <p>{{ props.selectedMailing?.emails }}</p>
+
+        <h3>Sender:</h3>
+        <p>{{ props.selectedMailing?.senderJSON.firstName }} {{ props.selectedMailing?.senderJSON.lastName }}</p>
+
+        <h3>Template ID:</h3>
+        <p>{{ props.selectedMailing?.templateId }}</p>
+
+        <h3>Status ID:</h3>
+        <p>{{ props.selectedMailing?.statusId }}</p>
+      </div>
+    </Card>
+  </Dialog>
+</template>
