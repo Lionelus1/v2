6e0f7987ad9378f6b214b0a2b243163741dab8cc
tq <tilekbergen.tiqu@gmail.com>
2024-11-05 12:06:06 +0500

    changes made for protocol

--- src/components/DocSignaturesInfo.vue
+++ src/components/DocSignaturesInfo.vue
@@ -122,7 +122,7 @@
                   :disabled="currentApprovalUsers.length < 1"/>
         </div>
       </TabPanel>
-      <TabPanel :header="$t('common.protocol')" v-if="isReport">
+      <TabPanel :header="$t('common.protocol')" v-if="showProtocol">
         <Files class="mb-3" v-if="canUploadProtocol" folder="workplan" :fileID="docInfo?.id"
                fileType="image/*, .pdf, .doc, .docx, .txt" @uploaded="fileUploaded" simple-upload="true"></Files>
         <div v-for="item in relatedFile" :key="item.id" class="mb-3">
@@ -217,8 +217,8 @@ export default {
       currentApprovalUsersLoading: true,
       uploadedFile: null,
       relatedFile: null,
-      isReport: false,
       canUploadProtocol: false,
+      showProtocol: false
     }
   },
 
@@ -361,11 +361,11 @@ export default {
       }).then(res => {
         if (res.data) {
           this.docInfo = res.data;
-          console.log(this.docInfo)
-          this.isReport = this.docInfo?.filePath.includes("report")
           this.getRelatedFiles();
           this.signatures = res.data.signatures;
-
+          if (this.docInfo?.docType === 26 || this.docInfo?.docType === 27) {
+            this.showProtocol = true;
+          }
           if (this.showAllSignsParam) {
             this.isShow = true;
           } else {
