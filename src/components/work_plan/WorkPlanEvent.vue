654df42e6f2c71f91e7ffbb842cb45852b13f1b2
TQ <tilekbergen.third@gmail.com>
2024-12-20 10:29:53 +0500

    changed for action button

--- src/components/work_plan/WorkPlanEvent.vue
+++ src/components/work_plan/WorkPlanEvent.vue
@@ -1283,48 +1283,83 @@ export default {
   }*/
   computed: {
     initItems() {
-      return [
-        {
-          label: this.$t('common.show'),
-          icon: 'fa-solid fa-eye',
-          disabled: !(this.isPlanApproved && this.canExecuteEvent),
-          visible: this.isFinish,
-          command: () => {
-            this.openPlanExecuteSidebar();
+      return (data) => {
+        // Проверка для научного плана
+        let curDate = moment(new Date());
+        let sDate = moment(new Date(data.start_date));
+        let fDate = moment(new Date(data.end_date));
+        let showRes = false;
+        if (this.isSciencePlan){
+          showRes = this.isFinish && (curDate.isSameOrAfter(sDate) && curDate.isSameOrBefore(fDate));
+
+          if(this.isPlanCreator || this.isCreator || this.isAdmin){
+            showRes = this.isFinish;
+          }
+        } else {
+          showRes = this.isFinish;
+        }
+        return [
+          {
+            label: this.$t('common.show'),
+            icon: 'fa-solid fa-eye',
+            disabled: !(this.isPlanApproved && this.canExecuteEvent),
+            visible: showRes,
+
+            command: () => {
+              this.openPlanExecuteSidebar();
+            },
           },
-        },
-        {
-          label: this.$t('common.add'),
-          icon: 'fa-solid fa-plus',
-          disabled: !(
-              this.isPlanCreator ||
-              this.isCreator ||
-              (this.isUserResp(this.selectedEvent?.user) && !this.isFinish)
-          ),
-          visible: !this.isFinish && !this.isMastersPlan && !this.isDoctorsPlan,
-          command: () => {
-            this.showDialog(this.dialog.add);
+          {
+            label: this.$t('common.add'),
+            icon: 'fa-solid fa-plus',
+            disabled: !(
+                this.isPlanCreator ||
+                this.isCreator ||
+                (this.isUserResp(this.selectedEvent?.user) && !this.isFinish)
+            ),
+            visible: !this.isFinish && !this.isMastersPlan && !this.isDoctorsPlan && !this.isWorkSchedule,
+            command: () => {
+              this.showDialog(this.dialog.add);
+            },
           },
-        },
-        {
-          label: this.$t('common.edit'),
-          icon: 'fa-solid fa-pen',
-          disabled: !((this.isPlanCreator || this.isCreator) && !this.isFinish),
-          visible: !this.isFinish,
-          command: () => {
-            this.showDialog(this.dialog.edit);
+          {
+            label: this.$t('common.addMember'),
+            icon: 'fa-solid fa-plus',
+            disabled: !(this.isPlanCreator || this.isCreator || this.isUserResp(this.selectedEvent?.user) && !this.isFinish),
+            visible: !this.isFinish && !this.isWorkSchedule && !this.isMastersPlan && !this.isDoctorsPlan,
+            command: () => {
+              this.showDialog(this.dialog.addMember)
+            }
           },
-        },
-        {
-          label: this.$t('common.delete'),
-          icon: 'fa-solid fa-trash',
-          disabled: !((this.isPlanCreator || this.isCreator) && !this.isFinish),
-          visible: !this.isFinish,
-          command: () => {
-            this.remove_event();
+          {
+            label: this.$t('common.edit'),
+            icon: 'fa-solid fa-pen',
+            disabled: !((this.isPlanCreator || this.isCreator) && !this.isFinish),
+            visible: this.isWorkSchedule ? !this.isFinish && (!findRole(null, 'student') && this.isWorkSchedule) : !this.isFinish,
+            command: () => {
+              this.showDialog(this.dialog.edit, false)
+            }
           },
-        },
-      ];
+          {
+            label: this.$t('workPlan.editRespUser'),
+            icon: 'fa-solid fa-pen',
+            disabled: !((this.isPlanCreator || this.isAdmin) && this.isPlanApproved),
+            visible: this.isPlanApproved && (this.isPlanCreator || this.isAdmin) && !this.isWorkSchedule,
+            command: () => {
+              this.showDialog(this.dialog.edit, true)
+            }
+          },
+          {
+            label: this.$t('common.delete'),
+            icon: 'fa-solid fa-trash',
+            disabled: !((this.isPlanCreator || this.isCreator) && !this.isFinish),
+            visible: this.isWorkSchedule ? !this.isFinish && (!findRole(null, 'student') && this.isWorkSchedule) : !this.isFinish,
+            command: () => {
+              this.remove_event();
+            },
+          },
+        ];
+      }
     },
     isSciencePlan() {
       return (
