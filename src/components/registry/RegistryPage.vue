252904a07fddc12f0b76f9d11aa6239cdbe4f388
saulet <mega.saulet@list.ru>
2025-01-22 14:42:36 +0500

    update

--- src/components/registry/RegistryPage.vue
+++ src/components/registry/RegistryPage.vue
@@ -36,7 +36,7 @@
         <!-- Изображение -->
         <div class="image-section">
           <div v-for="(param, index) in extractedDataType4" :key="index">
-            <h3 style="margin-top: 20px">{{ t('registry.img') }}</h3>
+            <h3 v-if="param.value_ru" style="margin-top: 20px">{{ t('registry.img') }}</h3>
             <img :src="'http://testsmart.enu.kz/api/serve?path=' + param.value_ru" alt="Изображение объекта" />
           </div>
         </div>
