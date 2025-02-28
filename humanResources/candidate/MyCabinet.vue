712596e81c87547e2e1963259a5450038970545f
Ilyas <kenesbarvi@mail.ru>
2025-02-28 17:34:56 +0500

    Удалены все файлы

--- humanResources/candidate/MyCabinet.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="grid formgrid">
-        <MyCabinetView  :readonly="false" customType="myAccount"/>
-    </div>
-</template>
-
-<script setup>
-    import MyCabinetView from "./MyCabinetView"
-</script>
