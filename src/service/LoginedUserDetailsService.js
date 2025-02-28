261f1fee7f2707a68e380f4241eff7c2e874bb4c
Ilyas <kenesbarvi@mail.ru>
2025-02-28 16:41:58 +0500

    Очистка кэша Git

--- src/service/LoginedUserDetailsService.js
+++
@@ -1,9 +0,0 @@
-
-export default class LoginedUserDetailsService {
-
-    static ADMINISTRATOR = 'news_administrator'
-    static MODERATOR = 'news_moderator'
-    static USER = 'USER'
-    static STUDENT = 'student'
-    static PUBLISHER = 'news_publisher'
-}
--- src/service/organization.service.js
+++
@@ -1,8 +0,0 @@
-import api from "./api";
-import {getFileHeader, getHeader} from "@/config/config";
-
-export class OrganizationService {
-    departments(data) {
-        return api.post("/departments", data, {headers: getHeader()});
-    }
-}
